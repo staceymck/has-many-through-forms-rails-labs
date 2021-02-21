172f20b35c99d72679b6f7e093b1bc144982dc0c
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-21 14:04:46 -0600

    commit tree c7e5ad776dbed01d5325b4197296f164bc99eb4b

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-
-
+  
+  def categories_attributes=(categories_attributes) #categories_attributes = {"0"=>{"name"=>"Humor"}}
+    categories_attributes.values.each do |category_attribute|  #categories_attributes.values = [{"name"=>"Humor"}]
+      unless category_attribute[:name].empty? #category_attribute = {"name"=>"Humor"}
+        category = Category.find_or_create_by(category_attribute) 
+        self.post_categories.build(category: category)
+      end
+    end
+  end
 end
